0cbf1166a9f276d81b62c5788d76ce4bbbc3066c
“junichi.noda” <jyunichi-noda@gmo.jp>
2015-12-03 13:59:02 +0900

    add 2

--- src/main/scala/ar_access_log_sample.scala
+++ src/main/scala/ar_access_log_sample.scala
@@ -3,7 +3,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.fpm.AssociationRules
 import org.apache.spark.mllib.fpm.FPGrowth.FreqItemset
 
-object ArSample {
+object ArSample2 {
   def main(args: Array[String]): Unit = {
 
 val conf = new SparkConf().setAppName("Ar Application")
@@ -11,13 +11,17 @@ val conf = new SparkConf().setAppName("Ar Application")
     val sc = new SparkContext(conf)
 
 val freqItemsets = sc.parallelize(Seq(
-  new FreqItemset(Array("a"), 15L),
-  new FreqItemset(Array("b"), 35L),
-  new FreqItemset(Array("a", "b"), 12L)
+  new FreqItemset(Array("http://maru/page1"), 20L),
+  new FreqItemset(Array("http://maru/page2"), 20L),
+  new FreqItemset(Array("http://maru/page3"), 20L),
+  new FreqItemset(Array("http://maru/page4"), 20L),
+  new FreqItemset(Array("http://maru/page5"), 20L),
+  new FreqItemset(Array("http://maru/page1", "http://maru/page2"), 19L),
+  new FreqItemset(Array("http://maru/page4", "http://maru/page5"), 18L)
 ));
 
 val ar = new AssociationRules()
-  .setMinConfidence(0.8)
+  .setMinConfidence(0.6)
 val results = ar.run(freqItemsets)
 
 results.collect().foreach { rule =>
